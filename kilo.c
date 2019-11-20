a9e673b655bc62d4c5f5711f70b1928e719372e4
Waqar Ahmed <waqar.17a@gmail.com>
2019-11-20 12:50:53 +0500

    Refactor and screen clearing

--- kilo.c
+++ kilo.c
@@ -3,16 +3,29 @@
 #include<ctype.h>
 #include<unistd.h>
 #include<termios.h>
+#include<errno.h>
+
+#define CTRL_KEY(key) (key & 0x1f)
+
+void die(const char *s) 
+{
+	perror(s); 
+	exit(1); 
+} 
 
 struct termios term;
 
 void disable_raw_mode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &term);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &term) == -1) {
+		die ("tcsetattr(&term): failed");
+	}
 }
 
 void enable_raw_mode()
 {
-	tcgetattr(STDIN_FILENO, &term);
+	if (tcgetattr(STDIN_FILENO, &term) == -1) {
+		die("tcgetattr(&term): failed");
+	}
 	atexit(disable_raw_mode);
 
 	struct termios raw = term;
@@ -29,23 +42,43 @@ void enable_raw_mode()
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+		die ("tcsetattr(&raw): failed");
+	}
 }
 
+void editor_refresh_screen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+char editor_read_key() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN)
+			die("read()");
+	}
+	return c;
+}
+
+void editor_process_keypress(){
+	char c = editor_read_key();
+	switch (c) {
+	case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
+		exit(0);
+		break;
+	}
+}
 
 int main()
 {
 	enable_raw_mode();
 	while(1) {
-		char c = '\0';
-		read(STDIN_FILENO, &c, 1);
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d - %c\r\n", c, c);
-		}
-
-		if (c == 'q') break;
+		editor_refresh_screen();
+		editor_process_keypress();
 	}
 	return 0;
 }
